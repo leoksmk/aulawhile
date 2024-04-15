3f869c63513046338bee04a4d741c1dd3ed39d3e
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-15 20:51:28 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# pontos = 0
+# questao = 1
+# while questao <= 3:
+#     resposta = input(f"Resposta da questão {questao}: ")
+#     if questao == 1 and resposta == "b":
+#          pontos = pontos + 1
+#     elif questao == 2 and resposta == "a":
+#          pontos = pontos + 1
+#     elif questao == 3 and resposta == "d":
+#          pontos = pontos + 1
+#     questao = questao + 1
+# print(f"O aluno fez {pontos} ponto(s).")
+
+#
+# n = 1 #contador
+# soma = 0 #acumulador
+# while n <= 10:
+#       x = float(input(f"Digite o número {n}: "))
+#       soma = soma + x
+#       n = n + 1
+# print(f"Soma = {soma:.2f}")
+# print(f"a media é {(soma/10):.2f}")
+
+float(input("entre com a taxa de juros"))
